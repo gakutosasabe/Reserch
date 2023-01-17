4151f7d1eff75aeba641adff9e3e271c7b6596f0
gakuto45 <gakuto0122@gmail.com>
2023-01-17 09:44:38 +0900

    身体拡張ビジョンの追加

--- BodyExpendReserch/開発/逆行列計算.py
+++ BodyExpendReserch/開発/逆行列計算.py
@@ -4,11 +4,24 @@ import numpy as np
 np.random.seed(0)
 
 #行列の大きさを設定
-L=2
+L=10
 M=3
-N=2
+N=3
 
 #ランダムな複素数のテンソルを定義する
 A=np.random.uniform(size = L*M*N)
 A=np.reshape(A,(L,M,N)) #行×列×奥
 print(A)
+
+#行列式を計算する
+detA = np.linalg.det(A)
+
+print("detA(0):", detA[0])
+
+det3A = np.linalg.det(3*A)
+
+#逆行列を計算する
+A_inv = np.linalg.inv(A)
+
+#Aとかけて単位行列になっているかどうかを検算
+AA_inv = np.einsum()
