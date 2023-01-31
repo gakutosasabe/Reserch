ddd726264b1e4207015256711efa5a1bbfba2e10
gakuto45 <gakuto0122@gmail.com>
2023-01-31 20:01:07 +0900

    Pyacoustics理解途中

--- BodyExpendReserch/開発/Pyacousticsでシミュレーション.py
+++ BodyExpendReserch/開発/Pyacousticsでシミュレーション.py
@@ -49,3 +49,39 @@ room_dim = np.r_[10.0,10.0,10.0]
 #マイクロホンアレイを置く部屋の場所
 mic_array_loc = room_dim / 2 + np.random.randn(3) * 0.1
 
+#マイクロホンアレイのマイク配置
+mic_alignments = np.array(
+    [
+        [-0.01, 0.0, 0.0],
+        [0.01, 0.0, 0.0],
+    ]
+)
+
+#マイクロホン数
+n_channels = np.shape(mic_alighments)[0] #今回は2
+
+#マイクロホンアレイの座標系
+R=mic_alignments .T +mic_array_loc[:,None]
+
+#部屋を生成する
+room = pa.ShoeBox(room_dim, fs=sample_rate, max_order=0)
+
+#用いるマイクロホンアレイの情報を設定する
+room.add_microphone_array(pa.MicrophoneArray(R, fs=room.fs))
+
+#音源の場所
+doas= np.array(
+    [[np.pi/2., 0],
+     [np.pi/2.,np.pi/2.]
+     ]
+)
+
+#音源とマイクロホンの距離
+distance = 1.
+source_locations = np.zeros((3, doas.shape[0]),dtype=doas.dtype)
+source_locations[0, :] = np.cos(doas[:,1]) * np.sin(doas[:, 0])
+source_locations[1, :] = np.sin(doas[:,1]) * np.sin(doas[:, 0])
+source_locations[2, :] = np.cos(doas[:,0]) 
+source_locations *= distance
+source_locations += mic_array_loc[:,None]
+
