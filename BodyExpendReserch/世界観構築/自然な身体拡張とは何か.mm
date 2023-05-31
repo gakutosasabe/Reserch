6b8671bfe1a43a39e92e7c75ba245be4bcca009d
gakuto45 <gakuto0122@gmail.com>
2023-05-31 10:10:25 +0900

    自然な人間拡張とは何か考え始めた

---
+++ BodyExpendReserch/世界観構築/自然な身体拡張とは何か.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1685495027916" ID="ID_580481330" MODIFIED="1685495049347" TEXT="&#x81ea;&#x7136;&#x306a;&#x8eab;&#x4f53;&#x62e1;&#x5f35;&#x3068;&#x306f;&#x4f55;&#x304b;">
+<node CREATED="1685495050875" ID="ID_767537421" MODIFIED="1685495058887" POSITION="right" TEXT="&#x80cc;&#x666f;">
+<node CREATED="1685495059823" ID="ID_1257669087" MODIFIED="1685495163887" TEXT="&#x3084;&#x306f;&#x308a;&#xff0c;&#x81ea;&#x7136;&#x3068;&#x6975;&#x529b;&#x5bfe;&#x7acb;&#x3057;&#x306a;&#x3044;&#x5f62;&#x3067;&#xff0c;&#x4eba;&#x9593;&#x306e;&#x30a8;&#x30b4;&#x3092;&#x5b9f;&#x73fe;&#x3059;&#x308b;&#x3053;&#x3068;&#x304c;&#xff0c;&#x5927;&#x4e8b;&#x306a;&#x6c17;&#x304c;&#x3059;&#x308b;"/>
+<node CREATED="1685495167170" ID="ID_493199299" MODIFIED="1685495269849" TEXT="&#x3068;&#x306a;&#x308b;&#x3068;&#xff0c;&#x81ea;&#x7136;&#x306a;&#x5f62;&#x3067;&#x306e;&#x8eab;&#x4f53;&#x62e1;&#x5f35;&#x3068;&#x306f;&#x4f55;&#x304b;&#xff1f;">
+<node CREATED="1685495307703" ID="ID_1223768778" MODIFIED="1685495337162" TEXT="&#x3053;&#x3053;&#x3092;&#x8003;&#x3048;&#x308b;&#x306e;&#x306b;&#x3042;&#x305f;&#x308a;&#xff0c;&#x4eba;&#x9593;&#x304c;&#x3059;&#x3067;&#x306b;&#x81ea;&#x7136;&#x306b;&#x884c;&#x3063;&#x3066;&#x3044;&#x308b;&#x8eab;&#x4f53;&#x62e1;&#x5f35;&#x306b;&#x3064;&#x3044;&#x3066;&#x8003;&#x3048;&#x3066;&#x307f;&#x308b;&#x3068;&#x3044;&#x3044;&#x306e;&#x3067;&#x306f;&#x306a;&#x3044;&#x304b;&#xff08;&#x5929;&#x91ce;&#x3055;&#x3093;&#xff09;"/>
+</node>
+<node CREATED="1685495270115" ID="ID_1410171459" MODIFIED="1685495295479" TEXT="&#x9006;&#x306b;&#x30a8;&#x30b4;&#x304c;&#x5f37;&#x3059;&#x304e;&#x3066;&#x9577;&#x7d9a;&#x304d;&#x3057;&#x306a;&#x3044;&#x8eab;&#x4f53;&#x62e1;&#x5f35;&#x3068;&#x306f;&#x4f55;&#x304b;&#xff1f;"/>
+</node>
+<node CREATED="1685495296664" ID="ID_1475249197" MODIFIED="1685495296664" POSITION="right" TEXT=""/>
+</node>
+</map>
